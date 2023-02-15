22ff808ef9ebe31b0c8f23ca2752511479286dd0
krzysztof.woelke <krzysiek@woelke.pl>
2023-02-15 14:15:35 +0100

    New springboot example for spring security package

---
+++ src/test/java/pl/woelke/webSec/WebSecApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.woelke.webSec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
